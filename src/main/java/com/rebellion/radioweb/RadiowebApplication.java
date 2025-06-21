8cf7fbb2c155fbf97ab40ce56ab71c06b39fdbe6
Rebellion <avinashee0012@gmail.com>
2025-06-21 18:14:02 +0530

    Finalizing bug fixes for push

--- src/main/java/com/rebellion/radioweb/RadiowebApplication.java
+++ src/main/java/com/rebellion/radioweb/RadiowebApplication.java
@@ -11,16 +11,4 @@ public class RadiowebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RadiowebApplication.class, args);
 	}
-
 }
-
-// TODO: BUG :: Data Filtration
-// TODO: BUG :: /stations/{name} is showing "Not Secure"
-// TODO: BUG (IN END) :: /stations Border to each station like stations/{name}
-
-// TODO: FEATURE :: listened count
-// TODO: FEATURE :: errorCount for stations
-// TODO: FEATURE :: ratings for stations
-// TODO: FEATURE :: Create a /addStation controller
-// TODO: FEATURE :: Backlink Generation Strategy
-// TODO: FEATURE :: /blogs for SEO --> 1. Top 10 stations of MMM YYYY

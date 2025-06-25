d17428d89a57f9e6a96ce0ad6fc394845c162cb1
Rebellion <avinashee0012@gmail.com>
2025-06-25 14:23:58 +0530

    Improved sitemap and included /blogs

--- src/main/java/com/rebellion/radioweb/repo/BlogRepo.java
+++ src/main/java/com/rebellion/radioweb/repo/BlogRepo.java
@@ -1,8 +1,10 @@
 package com.rebellion.radioweb.repo;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.rebellion.radioweb.entity.Blog;
@@ -10,4 +12,7 @@ import com.rebellion.radioweb.entity.Blog;
 @Repository
 public interface BlogRepo extends JpaRepository<Blog, Integer> {
     Optional<Blog> findByArticleUrl(String articleUrl);
+
+    @Query("SELECT b.articleUrl FROM Blog b")
+    List<String> findAllArticleUrls();
 }
